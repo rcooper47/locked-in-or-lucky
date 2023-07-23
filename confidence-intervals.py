58c4c9bd8694cc2784087112785dc3e29a161870
ryan cooper <3RCOOPER8@GMAIL.COM>
2023-07-22 20:10:18 -0400

    Init

---
+++ confidence-intervals.py
@@ -0,0 +1,9 @@
+"""
+scrape data from b-ball reference or something
+make confidence intervals for each player
+	- make for range (in feet)
+	- make for spot(ish)
+
+load data into database
+make api?
+"""
